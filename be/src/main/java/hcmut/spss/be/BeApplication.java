a406af8549e3687efe2da62e30db9546aa5dbfc5
KuaBK <tuanphonglqd@gmail.com>
2024-12-12 00:44:25 +0700

    final 4

--- be/src/main/java/hcmut/spss/be/BeApplication.java
+++ be/src/main/java/hcmut/spss/be/BeApplication.java
@@ -2,10 +2,12 @@ package hcmut.spss.be;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@ComponentScan(basePackages = {"*"})
 public class BeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BeApplication.class, args);
